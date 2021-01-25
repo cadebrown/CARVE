d9c216c386eecf1df531a1a5063dc5e3d9b03bb7
Gregory Croisdale <gtcroisdale1@gmail.com>
2021-01-24 19:04:43 -0500

    added float table and carve-data for better orgnaization

--- docs/js/carve.js
+++ docs/js/carve.js
@@ -52,7 +52,9 @@ loadlibcarve().then(function (_libcarve) {
 
         vert_drag($("#resize-console"), $("#main"), $("#console"));
 
-        genregtable($("#registers"));
+        genregtable($("#intreg-table"), CARVE_INTREG, "int");
+
+        genregtable($("#fltreg-table"), CARVE_FLTREG, "float");
 
         update_registers(state);
     })
@@ -65,54 +67,19 @@ function update_registers(s) {
     var dptr;
     for (var i = 0; i <= 31; ++i) {
         dptr = libcarve._carve_state_sri(s, i, 16)
-        $('#reg_x' + i.toString() + '_hex').text(libcarve.UTF8ToString(dptr))
+        $('#reg_' + "intx" + i.toString() + '_hex').text(libcarve.UTF8ToString(dptr))
         libcarve._free(dptr)
         dptr = libcarve._carve_state_sri(s, i, 10)
-        $('#reg_x' + i.toString() + '_dec').text(libcarve.UTF8ToString(dptr))
+        $('#reg_' + "intx" + i.toString() + '_dec').text(libcarve.UTF8ToString(dptr))
         libcarve._free(dptr)
     }
 }
 
 /* Generate the register table with appropriate IDs */
-function genregtable(elem) {
+function genregtable(elem, regs, type) {
     const genrow = (reg, abi, desc, saver) => {
-        return '<tr"><td class="reg_id">'+reg+'</td><td class="reg_name">'+abi+'</td><td class="reg_hex" id="reg_' + reg + '_hex">0</td><td class="reg_dec" id="reg_' + reg + '_dec">0</td></tr>';
+        return '<tr"><td class="reg_id">'+type+reg+'</td><td class="reg_name">'+abi+'</td><td class="reg_hex" id="reg_'+type+reg+'_hex">0</td><td class="reg_dec" id="reg_'+type+reg+'_dec">0</td></tr>';
     }
-
-    let regs = [
-        ["x0",  "zero",  "Hard-wired zero",                   "--"    ],
-        ["x1",  "ra",    "Return address ",                   "Caller"],
-        ["x2",  "sp",    "Stack pointer",                     "Callee"],
-        ["x3",  "gp",    "Global pointer",                    "--"],
-        ["x4",  "tp",    "Thread pointer",                    "--"],
-        ["x5",  "t0",    "Temporary/alternate link register", "Caller"],
-        ["x6",  "t1",    "Temporary register",                "Caller"],
-        ["x7",  "t2",    "Temporary register",                "Caller"],
-        ["x8",  "s0/fp", "Saved register/frame pointer",      "Callee"],
-        ["x9",  "s1",    "Saved register",                    "Callee"],
-        ["x10", "a0",    "Function argument/return value",    "Caller"],
-        ["x11", "a1",    "Function argument/return value",    "Caller"],
-        ["x12", "a2",    "Function argument",                 "Caller"],
-        ["x13", "a3",    "Function argument",                 "Caller"],
-        ["x14", "a4",    "Function argument",                 "Caller"],
-        ["x15", "a5",    "Function argument",                 "Caller"],
-        ["x16", "a6",    "Function argument",                 "Caller"],
-        ["x17", "a7",    "Function argument",                 "Caller"],
-        ["x18", "s2",    "Saved register",                    "Callee"],
-        ["x19", "s3",    "Saved register",                    "Callee"],
-        ["x20", "s4",    "Saved register",                    "Callee"],
-        ["x21", "s5",    "Saved register",                    "Callee"],
-        ["x22", "s6",    "Saved register",                    "Callee"],
-        ["x23", "s7",    "Saved register",                    "Callee"],
-        ["x24", "s8",    "Saved register",                    "Callee"],
-        ["x25", "s9",    "Saved register",                    "Callee"],
-        ["x26", "s10",   "Saved register",                    "Callee"],
-        ["x27", "s11",   "Saved register",                    "Callee"],
-        ["x28", "t3",    "Temporary register",                "Caller"],
-        ["x29", "t4",    "Temporary register",                "Caller"],
-        ["x30", "t5",    "Temporary register",                "Caller"],
-        ["x31", "t6",    "Temporary register",                "Caller"],
-    ];
 
     let out = "<table id='reg_table'>";
 
@@ -124,7 +91,7 @@ function genregtable(elem) {
 
     out += "</table>";
 
-    elem[0].innerHTML = out;
+    elem[0].innerHTML += out;
 }
 
 /* Inspired by w3 schools lesson                          */

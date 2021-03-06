c6ac1b6dd12deb8b69d5e622ecea9a7b9fd69b68
Cade Brown <brown.cade@gmail.com>
2021-03-06 16:23:07 -0500

    unhalted

--- src/exec.c
+++ src/exec.c
@@ -9,6 +9,7 @@
 int carve_exec(carve_state s, carve_prog p) {
     s->pc = (carve_int)p->inst;
 
+    s->is_halted = false;
     while (!s->is_halted) {
         carve_execinst(s, *(carve_inst*)s->pc);
         s->pc += sizeof(carve_inst);
